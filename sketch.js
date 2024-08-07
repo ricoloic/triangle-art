cee9d1fc609e31546cf6067e3884477d8d2a6c81
ricoloic <ricoloic4@gmail.com>
2024-08-06 23:44:20 -0400

    working

--- sketch.js
+++ sketch.js
@@ -1,81 +1,80 @@
 let palette;
 let graphic;
 
+const circleStroke = true;
+
 function setup() {
     graphic = createGraphics(window.innerWidth, window.innerHeight)
-    // graphic.stroke(255);
+    //graphic.stroke(0);
+    //graphic.strokeWeight(2);
     graphic.noStroke();
     graphic.angleMode(DEGREES);
     graphic.pixelDensity(1);
-    
+
     createCanvas(window.innerWidth, window.innerHeight);
     angleMode(DEGREES);
     pixelDensity(1);
 
-    palette = COLOR_PALETTES.happy.map((p) => p.color) // random(Object.values(COLOR_PALETTES)).map((p) => p.color);
-}
+    palette = COLOR_PALETTES.pastel2.map((p) => p.color) // random(Object.values(COLOR_PALETTES)).map((p) => p.color);
 
-function draw() {
-    if (frameCount <= 3000 && frameCount % 30 === 0) {
-        console.log(frameCount);
-        copy(graphic, 0, 0, width, height, 0, 0, width, height);
-    }
 
-    if (frameCount < 3000) {
+    for (let i = 0; i < 5000; i++) {
         graphic.push();
         graphic.translate(random(-80, width + 80), random(-80, height + 80));
         graphic.fill(random(palette));
         createTriangle();
         graphic.pop();
-    } else if (frameCount < 3040) {
+        copy(graphic, 0, 0, width, height, 0, 0, width, height);
+    }
+
+    for (let i = 0; i < 30; i++) {
         push();
-        rotate(random(360));
         copyCircle();
         pop();
     }
 }
 
 function copyCircle() {
-    let size = floor(random(85, 600));
-    let radius = size / 2;
+    let size = min(width, floor(random(85, 600)));
+    let radius = floor(size / 2);
     let cx = floor(random(radius, width - radius));
     let cy = floor(random(radius, height - radius));
 
-    const d = pixelDensity();
     let data = [];
-    graphic.loadPixels();
 
-    // Loop over the circle's bounding box
-    for (let x = cx - radius; x < cx + radius; x++) {
-        for (let y = cy - radius; y < cy + radius; y++) {
-            if (dist(x, y, cx, cy) > radius) continue;
+    const temp = createGraphics(size, size);
+    temp.pixelDensity(1);
+    temp.translate(radius, radius);
+    temp.rotate(random(TWO_PI));
+    temp.copy(graphic, cx - radius, cy - radius, size, size, -radius, -radius, size, size);
+    let d = temp.pixelDensity();
 
-            const index = 4 * ((y * d) * width * d + (x * d));
+    temp.loadPixels();
+    for (let x = 0; x < size; x++) {
+        for (let y = 0; y < size; y++) {
+            if (dist(x, y, radius, radius) > radius) continue;
+
+            const index = 4 * ((y * d) * temp.width * d + (x * d));
             data.push({
-                x: x - cx,
-                y: y - cy,
+                x: x - radius,
+                y: y - radius,
                 pixels: [
-                    graphic.pixels[index + 0],
-                    graphic.pixels[index + 1],
-                    graphic.pixels[index + 2],
-                    graphic.pixels[index + 3],
+                    temp.pixels[index + 0],
+                    temp.pixels[index + 1],
+                    temp.pixels[index + 2],
+                    temp.pixels[index + 3],
                 ],
             });
         }
     }
-    graphic.updatePixels();
+    temp.updatePixels();
 
+    d = pixelDensity();
     cx = floor(random(radius, width - radius));
     cy = floor(random(radius, height - radius));
-
-    const angle = random(TWO_PI);
-
     loadPixels();
     for (const pix of data) {
-        const rx = pix.x * cos(angle) - pix.y * sin(angle);
-        const ry = pix.x * sin(angle) + pix.y * cos(angle);
-
-        const index = 4 * (((cy + floor(ry)) * d) * width * d + ((cx + floor(rx)) * d));
+        const index = 4 * (((cy + pix.y) * d) * width * d + ((cx + pix.x) * d));
         if (index >= 0 && index < pixels.length) {
             pixels[index + 0] = pix.pixels[0];
             pixels[index + 1] = pix.pixels[1];
@@ -84,11 +83,18 @@ function copyCircle() {
         }
     }
     updatePixels();
+
+    if (circleStroke) {
+        stroke(255);
+        strokeWeight(3);
+        noFill();
+        circle(cx, cy, size);
+    }
 }
 
 function createTriangle() {
     graphic.rotate(30);
-    const size = random(80, 200);
+    const size = random(120, 300);
     const current = createVector();
     graphic.beginShape();
     graphic.vertex(current.x, current.y);
